362e5652cee82f07dc8a63fad327efc40584de97
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2018-03-12 14:35:05 -0600

    Versión final de LinkedListSet.

--- 5. Iteradores/src/mx/itesm/cem/iteradores/LinkedListSet.java
+++ 5. Iteradores/src/mx/itesm/cem/iteradores/LinkedListSet.java
@@ -1,6 +1,8 @@
 package mx.itesm.cem.iteradores;
 
 import java.util.AbstractSet;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Set;
@@ -16,8 +18,9 @@ public class LinkedListSet<E> extends AbstractSet<E> {
     private class LinkedListSetIterator
             implements Iterator<E> {
 
-        private Node<E> current = sentinel; 
-        
+        private Node<E> current = sentinel;
+        private boolean illegalState = true;
+
         @Override
         public boolean hasNext() {
             return current.next != sentinel;
@@ -29,12 +32,20 @@ public class LinkedListSet<E> extends AbstractSet<E> {
                 throw new NoSuchElementException();
             }
             current = current.next;
+            illegalState = false;
             return current.info;
         }
 
         @Override
         public void remove() {
-            throw new UnsupportedOperationException();
+            if (illegalState) {
+                throw new IllegalStateException();
+            }
+            current.next.prev = current.prev;
+            current.prev.next = current.next;
+            current = current.prev;
+            size--;
+            illegalState = true;
         }
     }
 
@@ -45,6 +56,11 @@ public class LinkedListSet<E> extends AbstractSet<E> {
         sentinel = new Node<>();
         sentinel.next = sentinel;
         sentinel.prev = sentinel;
+    }
+
+    public LinkedListSet(Collection<E> coll) {
+        this();
+        addAll(coll);
     }
 
     @Override
@@ -74,7 +90,7 @@ public class LinkedListSet<E> extends AbstractSet<E> {
         size++;
         return true;
     }
-    
+
     public static void main(String[] args) {
         Set<String> s = new LinkedListSet<>();
         s.add("A");
@@ -82,5 +98,14 @@ public class LinkedListSet<E> extends AbstractSet<E> {
         System.out.println(s);
         s.add("A");
         System.out.println(s);
+        Set<String> s2 = new LinkedListSet<>(
+                Arrays.asList("a", "b", "c", "d"));
+        System.out.println(s2);
+        s2.clear();
+        System.out.println(s2);
+        s2.add("x");
+        System.out.println(s2);
+        s2.remove("x");
+        
     }
 }
